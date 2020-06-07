a6bb101902f5b6206a0c5f11e73e0bab081f0f67
Luke Williams <l.williams@derby.ac.uk>
2020-06-07 15:39:06 +0100

    Limit SSH and console access to the public IP where the lab was provisioned

--- vpc.tf
+++ vpc.tf
@@ -56,14 +56,14 @@ resource "aws_security_group" "deepfence-lab-sg" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${chomp(data.http.icanhazip.body)}/32"]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${chomp(data.http.icanhazip.body)}/32"]
   }
 
   ingress {
